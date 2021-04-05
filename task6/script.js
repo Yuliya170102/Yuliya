41c252a64ca7d1e820fa04652ab4019aa5b8a4f0
Юлия <yuliya453464@mail.ru>
2021-04-05 23:23:06 +0300

    change new task6

---
+++ task6/script.js
@@ -0,0 +1,508 @@
+let adList = [
+
+ {
+
+   id: '1',
+   description: 'Скидка на женский спортивный костюм -20%',
+   createdAt: new Date('2021-03-16T23:00:00'),
+   link: 'https://sisters.by/catalog/clothing/sportivnye-kostyumy/komplekt-20-24-2.html',
+   vendor: 'Sisters',
+   photoLink: 'https://sisters.by/_thumbs/items-product_page_item_mobile/icon-147870.jpg',
+   validUntil: new Date('2021-04-16T23:00:00'),
+   discount: '20%',
+   hashTags: ['одежда'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+{
+
+   id: '2',
+   description: 'Скидка на свитер -58%',
+   createdAt: new Date('2021-03-01T23:00:00'),
+   link: 'https://www.lamoda.by/p/by004ewllwn1/clothes-byswan-sviter/',
+   vendor: 'lamoda',
+   photoLink: 'https://a.lmcdn.ru/img600x866/B/Y/BY004EWLLWN1_12690724_2_v1_2x.jpg',
+   validUntil: new Date('2021-03-24T23:00:00'),
+   discount: '58%',
+   hashTags: ['одежда'],
+   rating: 4.6,
+   reviews :[]
+
+ },
+ {
+
+   id: '3',
+   description: 'Скидка на блузку -40%',
+   createdAt: new Date('2021-03-16T23:00:00'),
+   link: 'https://sisters.by/catalog/clothing/bluzki-i-rubashki/bluzka-vit-21-1611x.html',
+   vendor: 'Sisters',
+   photoLink: 'https://sisters.by/_thumbs/items-product_page_item_mobile/icon-148023.jpg',
+   validUntil: new Date('2021-04-16T23:00:00'),
+   discount: '40%',
+   hashTags: ['одежда'],
+   rating: 3.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '4',
+   description: 'Скидка на стайлер POLARIS -45%',
+   createdAt: new Date('2021-03-01T23:00:00'),
+   link: 'https://sila.by/bt/stajlery/POLARIS/phs_2405k_',
+   vendor: 'Электросила',
+   photoLink: 'https://sila.by/img/catalog2015/bt/tovar70534.jpg',
+   validUntil: new Date('2021-04-15T23:00:00'),
+   discount: '45%',
+   hashTags: ['техника'],
+   rating: 4.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '5',
+   description: 'Скидка на вытяжку BOSCH -20%',
+   createdAt: new Date('2021-02-27T23:00:00'),
+   link: 'https://sila.by/vt/vytyazhki/BOSCH/dhi642eq',
+   vendor: 'Электросила',
+   photoLink: 'https://sila.by/img/catalog2015/vt/tovar69256.jpg',
+   validUntil: new Date('2021-03-31T23:00:00'),
+   discount: '20%',
+   hashTags: ['техника'],
+   rating: 3.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '6',
+   description: 'Скидка на книгу "Взламывая мозг" -7%',
+   createdAt: new Date('2021-03-01T23:00:00'),
+   link: 'https://oz.by/books/more10976092.html',
+   vendor: 'OZ',
+   photoLink: 'https://s2-goods.ozstatic.by/2000/92/976/10/10976092_0.jpg',
+   validUntil: new Date('2021-03-30T23:00:00'),
+   discount: '7%',
+   hashTags: ['книга'],
+   rating: 0.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '7',
+   description: 'Скидка на книгу "Метро 2033" -20%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://oz.by/books/more10796109.html',
+   vendor: 'OZ',
+   photoLink: 'https://s5-goods.ozstatic.by/2000/109/796/10/10796109_0.jpg',
+   validUntil: new Date('2021-04-28T23:00:00'),
+   discount: '20%',
+   hashTags: ['книга'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '8',
+   description: 'Скидка на гель для душа -35%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://oz.by/showergels/more10843275.html',
+   vendor: 'OZ',
+   photoLink: 'https://s3-goods.ozstatic.by/2000/275/843/10/10843275_0.jpg',
+   validUntil: new Date('2021-04-30T23:00:00'),
+   discount: '35%',
+   hashTags: ['косметика'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '9',
+   description: 'Скидка на нож -15%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://oz.by/knives/more10967891.html',
+   vendor: 'OZ',
+   photoLink: 'https://s1-goods.ozstatic.by/2000/891/967/10/10967891_0.jpg',
+   validUntil: new Date('2021-03-28T23:00:00'),
+   discount: '15%',
+   hashTags: ['посуда'],
+   rating: 0.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '10',
+   description: 'Скидка на матрас -46%',
+   createdAt: new Date('2021-03-03T23:00:00'),
+   link: 'https://pinskdrev.by/catalog/tolko-onlayn/matrasyi-mayya---sale/',
+   vendor: 'Пинскдрев',
+   photoLink: 'https://pinskdrev.by/web/files/imagick_cache/w580h580t3/web/catalogfiles/catalog/products/66df0686cefd8923ece4c7c790b4dafa.jpg',
+   validUntil: new Date('2021-06-15T23:00:00'),
+   discount: '46%',
+   hashTags: ['матрасы'],
+   rating: 0.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '11',
+   description: 'Скидка на игру "Grand Theft Auto V" -58%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://hot-game.info/game/grand-theft-auto-5',
+   vendor: 'Hot Game',
+   photoLink: 'https://hot-game.info/uploads/media/game/0001/10/thumb_9474_game_poster.jpeg',
+   validUntil: new Date('2021-08-30T23:00:00'),
+   discount: '58%',
+   hashTags: ['игры'],
+   rating: 4.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '12',
+   description: 'Скидка на игру "Playerunknown Battlegrounds" -19%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://hot-game.info/game/Playerunknowns-Battlegrounds',
+   vendor: 'Hot Games',
+   photoLink: 'https://hot-game.info/uploads/media/game/0001/07/thumb_6545_game_poster2.jpeg',
+   validUntil: new Date('2021-06-30T23:00:00'),
+   discount: '19%',
+   hashTags: ['игры'],
+   rating: 4.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '13',
+   description: 'Скидка на игру "S.T.A.L.K.E.R.: Bundle" -90%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://hot-game.info/game/STALKER-Bundle',
+   vendor: 'Hot Games',
+   photoLink: 'https://hot-game.info/uploads/media/game/0001/34/thumb_33884_game_poster.jpeg',
+   validUntil: new Date('2021-04-20T23:00:00'),
+   discount: '90%',
+   hashTags: ['игры'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '14',
+   description: 'Скидка на мягкую игрушку -15%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://buslik.by/catalog/igrushki_i_igry/myagkie_igrushki/myagkie_zverushki/osminog_v_assort_malvina_15_141_1_6/',
+   vendor: 'Буслик',
+   photoLink: 'https://buslik.by/upload/slam.image/iblock/589/5899e80c097d5ba77cc6467364b6c6ce-90.jpg',
+   validUntil: new Date('2021-04-23T23:00:00'),
+   discount: '15%',
+   hashTags: ['игрушки'],
+   rating: 0.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '15',
+   description: 'Скидка на куклу -50%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://buslik.by/catalog/igrushki_i_igry/kukly_i_aksessuary/kukly_1/mini_kukly/kukla_dong_huan_big_tree_toys_12sm/',
+   vendor: 'Буслик',
+   photoLink: 'https://buslik.by/upload/slam.image/iblock/a86/a86cec311e22d4396acf8b86e9f5efec-90.jpg',
+   validUntil: new Date('2021-05-10T23:00:00'),
+   discount: '50%',
+   hashTags: ['игрушки'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '16',
+   description: 'Скидка на LEGO -15%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://buslik.by/catalog/igrushki_i_igry/konstruktory/lego/konstruktor_kosmicheskiy_robot_dlya_gornykh_rabot_327_detaley_lego_creator_31115/',
+   vendor: 'Буслик',
+   photoLink: 'https://buslik.by/upload/slam.image/iblock/ec4/ec4f0a6d7445e05aa2dd7bfb3a61306f-90.jpg',
+   validUntil: new Date('2021-03-30T23:00:00'),
+   discount: '15%',
+   hashTags: ['игрушки'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '17',
+   description: 'Скидка на витамины -50%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://www.iherb.com/pr/California-Gold-Nutrition-Andrographis-Immune-with-AP-BIO-100-mg-120-Tablets/101059',
+   vendor: 'iHerb',
+   photoLink: 'https://s3.images-iherb.com/cgn/cgn01819/v/10.jpg',
+   validUntil: new Date('2021-04-15T23:00:00'),
+   discount: '50%',
+   hashTags: ['здоровье'],
+   rating: 4.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '18',
+   description: 'Скидка на витамины -50%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://www.iherb.com/pr/Now-Foods-High-Potency-Vitamin-D-3-125-mcg-5-000-IU-120-Softgels/10421',
+   vendor: 'iHerb',
+   photoLink: 'https://s3.images-iherb.com/now/now00372/v/27.jpg',
+   validUntil: new Date('2021-05-04T23:00:00'),
+   discount: '50%',
+   hashTags: ['здоровье'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '19',
+   description: 'Скидка на женские джинсы -36%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://www.wildberries.ru/catalog/15537164/detail.aspx?targetUrl=BP',
+   vendor: 'ТВОЕ',
+   photoLink: 'https://images.wbstatic.net/c516x688/new/15530000/15537164-1.jpg',
+   validUntil: new Date('2021-06-12T23:00:00'),
+   discount: '36%',
+   hashTags: ['одежда'],
+   rating: 4.0,
+   reviews :[]
+
+ },
+ {
+
+   id: '20',
+   description: 'Скидка на платье -12%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://www.wildberries.ru/catalog/19126078/detail.aspx?targetUrl=BP',
+   vendor: 'ТВОЕ',
+   photoLink: 'https://images.wbstatic.net/c516x688/new/19120000/19126078-1.jpg',
+   validUntil: new Date('2021-03-25T23:00:00'),
+   discount: '12%',
+   hashTags: ['одежда'],
+   rating: 5.0,
+   reviews :[]
+
+ },
+ ]
+
+ let getAds = (function(skip = 0, top = 10, filterConfig = {}){
+    let adsResult = [];
+    if (Object.keys(filterConfig).length !== 0 && filterConfig.constructor === Object) {
+        let key = Object.keys(filterConfig);
+        let value = filterConfig[key[0]];
+        if (key[0] === 'createdAt' || key[0] === 'validUntil') {
+            adList.forEach(elem => {
+                let timeRequest = String(new Date(value));
+                let time = String(elem[key]);
+                if (time == timeRequest)
+                    resultAds.push(elem);
+            });
+        } else {
+            adList.forEach(elem => {
+                if (elem[key] === value)
+                    resultAds.push(elem);
+            });
+        }
+
+        if (resultAds.length !== 0) {
+            resultAds.sort(function (a, b) {
+                return new Date(a.createdAt) - new Date(b.createdAt);
+            });
+        }
+    } else {
+        resultAds = adList.slice();
+        resultAds.sort(function (a, b) {
+            return new Date(a.createdAt) - new Date(b.createdAt);
+        });
+    }
+
+    if (resultAds.length !== 0) {
+        let border = skip + top;
+        if (border > resultAds.length)
+            resultAds = resultAds.slice(skip, resultAds.length);
+        else
+            resultAds = resultAds.slice(skip, border);
+    }
+    return adsResult;
+});
+
+ let getAd = (function(id){
+    for (let i =0; i<adList.length;++i){
+        if(adList[i].id == id){
+            return adList[i];
+        }
+    }
+    return {};
+});
+
+let validateAd = (function(adItem){
+    if (typeof(adItem.id) == 'string' &&
+    typeof(adItem.description) == 'string' && adItem.description.length < 200  &&
+    adItem.createdAt instanceof Date &&  
+    typeof(adItem.link) == 'string' &&
+    typeof(adItem.vendor) == 'string' && adItem.vendor.length != 0 &&
+    adItem.hashTags instanceof Array &&
+    typeof(adItem.discount) == 'string' &&  adItem.discount.length != 0 &&
+    adItem.validUntil instanceof Date){
+        return true;
+    }else{
+        return false;
+    }
+});
+
+let addAd = (function(adItem){
+    if (adList.findIndex(ad => ad.id === adItem.id) >= 0) {
+        return false;
+    }
+   if (validateAd(adItem)) {
+        adList.push(adItem);
+         return true;
+    } else {
+        return false;
+    }
+});
+
+let editAd = (function(id,adItem){
+   let keys = Object.keys(adItem);
+    if(keys.includes('id') || keys.includes('author') || keys.includes('createdAt')){
+        return false;
+    }
+
+    let ind;
+    for(let i=0;i<=adList.length;++i){
+        if(i == adList.length){
+            return false;
+        }else if(adList[i].id === id){
+            ind = i;
+            break;
+        }
+    }
+    new_ad = adList[ind];
+    for(let i=0;i<=adList.length;++i){
+        new_ad[keys[i]] = adItem[keys[i]];
+    }
+    if(validateAd(new_ad)){
+        adList[ind] = new_ad;
+        return true;
+    }else{
+        return false;
+    }
+  });
+
+let removeAd = (function(id){
+    for (let i = 0;i<adList.length;++i){
+        if(adList[i].id === id){
+            adList.splice(i,1);
+            return true;
+        }
+    }
+    return false;
+});
+
+
+
+console.log('getAds');
+console.log(getAds()); 
+console.log(getAds(0, 10))
+console.log(getAds(0,10,{vendors : ['OZ','Sisters']}));
+console.log(getAds(0,10,{hashTags : ['игры']}));
+
+console.log('getAd');
+console.log(getAd(50));
+console.log(getAd(3));
+
+
+console.log('validateAd');
+console.log(validateAd({
+   id: '17',
+   description: 'Скидка на витамины -50%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://www.iherb.com/pr/California-Gold-Nutrition-Andrographis-Immune-with-AP-BIO-100-mg-120-Tablets/101059',
+   vendor: 'iHerb',
+   photoLink: 'https://s3.images-iherb.com/cgn/cgn01819/v/10.jpg',
+   validUntil: new Date('2021-04-15T23:00:00'),
+   discount: '50%',
+   hashTags: ['здоровье'],
+   rating: 4.0,
+   reviews :[]
+}));//true
+
+console.log(validateAd({
+   description: 'Скидка на матрас -46%',
+   createdAt: new Date('2021-03-03T23:00:00'),
+   link: 'https://pinskdrev.by/catalog/tolko-onlayn/matrasyi-mayya---sale/',
+   vendor: 'Пинскдрев',
+   validUntil: new Date('2021-06-15T23:00:00'),
+   discount: '46%',
+   hashTags: ['матрасы'],
+   rating: 0.0,
+   reviews :[]
+}))//false
+console.log(validateAd({
+   description: 'Скидка на игру "Playerunknown Battlegrounds" -19%',
+   createdAt: new Date('2021-03-15T23:00:00'),
+   link: 'https://hot-game.info/game/Playerunknowns-Battlegrounds',
+   vendor: 'Hot Games',
+   photoLink: 'https://hot-game.info/uploads/media/game/0001/07/thumb_6545_game_poster2.jpeg',
+   validUntil: new Date('2021-06-30T23:00:00'),
+   discount: '19%',
+   hashTags: ['игры'],
+   rating: 4.0,
+   reviews :[]
+}))//false
+
+console.log('addAd');
+console.log(addAd({
+    id: '21',
+    description: 'Скидка на шариковую ручку 17%',
+    createdAt: new Date('2021-03-15T23:00:00'),
+    link: 'https://oz.by/pens/more10846624.html',
+    vendor: 'OZ',
+    photoLink: 'https://s3-goods.ozstatic.by/2000/624/846/10/10846624_0.jpg',
+    validUntil: new Date('2021-05-06T23:00:00'),
+    discount: '17%',
+    hashTags: ['канцтовары'],
+    rating: 5.0,
+    reviews:[]
+}));//true
+console.log(getAd(21));
+console.log(addAd({
+      id: '2',
+    description: 'Скидка на шариковую ручку 17%',
+    createdAt: new Date('2021-03-15T23:00:00'),
+    link: 'https://oz.by/pens/more10846624.html',
+    vendor: 'OZ',
+    photoLink: 'https://s3-goods.ozstatic.by/2000/624/846/10/10846624_0.jpg',
+    validUntil: new Date('2021-05-06T23:00:00'),
+    discount: '17%',
+    hashTags: ['канцтовары'],
+    rating: 5.0,
+    reviews:[]
+}));//false
+
+
+console.log('editAd');
+console.log(editAd('1',{discount:'3%'}));//true
+console.log(editAd('23',{id:3}));//false 
+console.log(editAd('1',{description:'Some random description', link : 'random.org'}));//true
+console.log(getAd(1));
+console.log('removeAd');
+console.log(removeAd('100'));//false
+console.log(removeAd('1'));//true
